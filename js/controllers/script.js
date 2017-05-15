6bbf90bb192ca00a235f8a36ceba08b99e8c8cae
Roman <ryzhakov.r@mail.ru>
2017-05-15 18:31:24 +0300

    first commit

---
+++ js/controllers/script.js
@@ -0,0 +1,33 @@
+myApp.controller("phoneCtrl", function ($scope) {
+
+    $scope.question={
+        text:   'Какой js-фреймворк лучше использовать?',
+        author: 'Иван Иванов',
+        date:   '20/10/2013',
+        answers:[{
+            text:   'AngularJS!',
+            author: 'Вова Сидоров',
+            date:   '20/10/2013',
+            rate:2
+        },{
+            text:   'AngularJS лучше всех',
+            author: 'Олег Кузнецов',
+            date:   '20/10/2013',
+            rate:1
+        },{
+            text:   'Я бы использовал knockout',
+            author: 'Неизвестный',
+            date:   '21/10/2013',
+            rate:0
+        }]
+    };
+
+    $scope.voteUp = function (answer){
+        answer.rate++;
+    };
+    $scope.voteDown = function (answer){
+        answer.rate--;
+    };
+
+});
+
