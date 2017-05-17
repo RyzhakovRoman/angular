366599c114f7122705976cdfaab990f84521620a
Roman <ryzhakov.r@mail.ru>
2017-05-17 19:12:42 +0300

    dispatch data

--- js/controllers/script.js
+++ js/controllers/script.js
@@ -1,43 +1,15 @@
-myApp.controller("phoneCtrl", function ($scope) {
+myApp.controller("phoneCtrl",
+function QuestionController($scope, $http) {
 
-    $scope.question={
-        text: 'Какой js-фреймворк лучше использовать?',
-        author: 'Иван Иванов',
-        date: '20/10/2013',
-        answers:
-            [{
-                text: 'AngularJS!',
-                author: 'Вова Сидоров',
-                date: '20/10/2013',
-                rate:2
-            },{
-                text: 'AngularJS лучше всех',
-                author: 'Олег Кузнецов',
-                date: '21/10/2013',
-                rate:3
-            },{
-                text: 'фигасе квестшен',
-                author: 'Неизвестный',
-                date: '22/10/2013',
-                rate:0
-            }]
+    $scope.response = {};
+    $scope.save = function (answer, answerForm){
+        if(answerForm.$valid){
+
+            $http.post("question.json", answer).then(function success (response) {
+                $scope.response = answer;//response.data;
+                console.log(response);
+            });
+        }
     };
 
-    $scope.voteUp = function (answer){
-        answer.rate++;
-    };
-    $scope.voteDown = function (answer){
-        answer.rate--;
-    };
 });
-
-// myApp.controller("answerCtrl", function ($scope) {
-//
-//         $scope.save = function (answer, answerForm){
-//             console.dir(answerForm);
-//             if(answerForm.$valid){
-//                 // действия по сохранению
-//                 alert(answer.author + ", ваш ответ сохранен");
-//             }
-//         };
-// });
